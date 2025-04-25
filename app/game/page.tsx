bf5966cb188b7d20e369bac68dac4a1221894d2f
Fraustiz <jules.drevon@gmail.com>
2025-04-25 21:36:31 +0200

    Refactor Game component layout for improved structure and styling; update iframe title and attributes

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -3,18 +3,20 @@ import Footer from "@/app/components/footer";
 
 export default function Game() {
   return (
-    <div className="bg-gray-900 flex flex-col h-dvh">
-      <Navbar />
-      <main className="h-dvh w-full p-10">
+    <div className="flex flex-col h-screen bg-gray-900">
+      <Navbar/>
+      <main className="flex-1 overflow-hidden p-10">
         <iframe
           src="/engine/index.html"
-          title="Godot Game"
-          className="border-3 border-gray-700 w-full h-full rounded-lg"
+          title="Erebos Game"
+          className="w-full h-full border-3 border-gray-700 rounded-lg"
           frameBorder="0"
           allowFullScreen
-        ></iframe>
+          scrolling="no"
+        />
       </main>
-      <Footer />
+      <Footer/>
     </div>
   );
 }
+
